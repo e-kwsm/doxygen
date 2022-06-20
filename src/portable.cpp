5a6a30e73adb39d838da652a29a2611aa1b0b54f
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2022-06-20 12:23:04 +0900

    modernize-use-nullptr

--- src/portable.cpp
+++ src/portable.cpp
@@ -168,7 +168,7 @@ int Portable::system(const DString &command,const DString &args,bool commandHasC
   }
   if (pid==0)
   {
-    const char * const argv[4] = { "sh", "-c", fullCmd.data(), 0 };
+    const char * const argv[4] = { "sh", "-c", fullCmd.data(), nullptr };
     execve("/bin/sh",const_cast<char * const*>(argv),environ);
     exit(127);
   }
